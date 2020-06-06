b641ce5af6596ddc0e13d3f76181b57483ae7e4b
valkineles <valkineles@gmail.com>
2020-06-06 09:27:45 -0300

    first commit

---
+++ src/pages/Points/index.tsx
@@ -0,0 +1,186 @@
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import MapVeiw, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+
+import api from '../../services/api';
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+const Points = () => {
+  const navigation = useNavigation();
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get('items').then((res) => {
+      setItems(res.data);
+    });
+  }, []);
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  function handleNavigateDetail() {
+    navigation.navigate('Detail');
+  }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapVeiw
+            style={styles.map}
+            initialRegion={{
+              latitude: -16.662528,
+              longitude: -49.2306432,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            <Marker
+              style={styles.mapMarker}
+              onPress={handleNavigateDetail}
+              coordinate={{
+                latitude: -16.662528,
+                longitude: -49.2306432,
+              }}
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image
+                  style={styles.mapMarkerImage}
+                  source={{
+                    uri:
+                      'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+                  }}
+                />
+                <Text style={styles.mapMarkerTitle}>Estrada</Text>
+              </View>
+            </Marker>
+          </MapVeiw>
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 20 }}>
+          {items.map((item) => (
+            <TouchableOpacity key={String(item.id)} style={styles.item} onPress={() => {}} activeOpacity={0.6}>
+              <SvgUri width={24} height={42} uri={item.image_url} />
+              <Text style={styles.itemTitle}>{item.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center',
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 0,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
+
+export default Points;
